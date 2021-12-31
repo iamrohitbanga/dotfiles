a2a282850f89904198af6528928df6d40c7d80ca
Rohit Banga <iamrohitbanga@gmail.com>
2021-12-31 23:09:40 +0000

    Move date module to the right in polybar

--- xmonad/copy_config.sh
+++ xmonad/copy_config.sh
@@ -2,5 +2,7 @@
 
 cp xmonad.hs ~/.xmonad/xmonad.hs
 cp xmobarrc ~/.xmobarrc
+mkdir -p ~/.config/polybar/scripts
 cp polybar/config ~/.config/polybar/config
 cp polybar/launch.sh ~/.config/polybar/launch.sh
+cp polybar/scripts/* ~/.config/polybar/scripts/
