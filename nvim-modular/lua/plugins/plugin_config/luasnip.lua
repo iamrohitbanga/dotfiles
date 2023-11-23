e87f291c09298b85f04a873865713826b76f0cc5
Rohit Banga <iamrohitbanga@gmail.com>
2023-11-23 21:52:43 +0000

    Add luasnip configuration

---
+++ nvim-modular/lua/plugins/plugin_config/luasnip.lua
@@ -0,0 +1,7 @@
+return {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!) - fails on submodule fetch, hence disable
+    -- build = "make install_jsregexp"
+}
